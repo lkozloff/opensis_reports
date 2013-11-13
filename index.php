7a9a9186be882c788cb9540112895b61550cc01f
lkozloff <lyle@asianhope.org>
2013-11-13 08:50:16 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+    <title>AH Student Report Portal</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <style>
+        body {
+           padding-top: 50px;
+        }
+        .starter-template {
+           padding: 40px 15px;
+           text-align: center;
+        }
+        iframe{
+            width: 100%;
+            height: 1000px;
+        }
+    </style>
+  </head>
+
+  <body>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><img src = "img/ahed.png"/></a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#demographics">Demographics</a></li>
+            <li><a href="#achievement">Achievement</a></li>
+            <li><a href="#attendance">Attendance</a></li>
+            <li><a href="#health">Health</a></li>
+            <li><a href="#contacts">Contacts</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Asian Hope Education</h1>
+        <p>
+            Care to get started?
+            <form>
+                <fieldset class='textbox'>
+                    <input type ="text" placeholder="Username">
+                    <input type="password" placeholder="Password"><br/><br/>
+                    <button type="button" class="btn btn-default">Submit</button>
+                </fieldset>
+            </form>
+        </p>
+        <p style="font-size: x-small;">Student information is private! Please be mindful.</p>
+        <p style="font-size: x-small;">(c) Asian Hope 2013</p>
+      </div>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-2.0.3.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
